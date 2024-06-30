e4629433b33105b6071de7b10d7692a8ab4798e3
Sachin-Yelne <sachin.a.yelne@gmail.com>
2024-06-30 13:19:03 -0400

    created sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,7 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-import jwt from 'jsonwebtoken'
-import { userInfo } from 'os';
-
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res, next) => {
@@ -20,8 +18,10 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
+    console.log('Email received:', email); // Log the email value
     try {
-        const validUser = await User.findOne({email});
+        
+        const validUser = await User.findOne({ email: email });
         if (!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Incorrect credentials'));
