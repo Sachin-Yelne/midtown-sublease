9bff77b9ac839be7f3d016b1648fa5a4aeb928da
Sachin-Yelne <sachin.a.yelne@gmail.com>
2024-07-06 13:42:34 -0400

    Home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-secondary'>About Midtown Housing</h1>
+      <p className='mb-4 text-dark-gray'>
+       Midtown Housing is a platform where people in Midtown Atlanta can easily sublease and sublet their housing. 
+       Our mission is to provide a seamless and efficient experience for both sublessors and sublessees, 
+       making the process of finding or offering a temporary stay as simple and stress-free as possible.</p>
+       <p className='mb-4 text-dark-gray'>
+       At Midtown Housing, we understand the unique needs of our community. 
+       Whether you're a student, a professional, or someone in transition, our goal is to connect you with the
+       right housing solutions quickly and effortlessly. We are committed to fostering a supportive and connected
+       community where everyone can find their perfect place in the beautiful Midtown Atlanta.</p>
+       <p className='mb-4 text-dark-gray'>
+       Our platform offers a user-friendly interface, reliable listings, and dedicated customer support to ensure
+       you have the best possible experience. Join Midtown Housing today and discover the ease of subleasing and
+       subletting in one of Atlanta's most vibrant neighborhoods.
+       </p>
+       <p className='mb-4 text-dark-gray'>If you have any questions or concerns, feel free to reach out to <a className= 'text-blue-500 hover:underline'href="mailto:sachin.a.yelne@gmail.com">sachin.a.yelne@gmail.com</a></p>
+
+
+
+
+      
+    </div>
   )
 }
